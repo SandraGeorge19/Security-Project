7245ce674bcba46ef9e70dfeff704a6e5a873c1a
Sandra George <georgesandra592@gmail.com>
2019-11-15 21:47:13 +0200

    file manager && class person && class patient && class Admin && simple switch case in main to ensure that files are read and write

---
+++ Security_Project/src/security_project/Admins.java
@@ -0,0 +1,78 @@
+
+package security_project;
+
+import java.util.ArrayList;
+
+
+public class Admins extends Person{
+    private String passwoed;
+    private String re_passwoed;
+    
+    private final String admins_FileName = "Admins.txt";
+    
+    public static ArrayList<Admins> admins = new ArrayList<Admins>();
+
+    public Admins() {
+    }
+
+    public Admins(String FName, String LName, String gender, String birthDate, int age, String phone, String address,String passwoed, String re_passwoed) {
+        
+        super( FName,  LName,  gender,  birthDate,  age,  phone,  address);
+        
+        this.passwoed = passwoed;
+        this.re_passwoed = re_passwoed;
+    }
+
+    public String getPasswoed() {
+        return passwoed;
+    }
+
+    public void setPasswoed(String passwoed) {
+        this.passwoed = passwoed;
+    }
+
+    public String getRe_passwoed() {
+        return re_passwoed;
+    }
+
+    public void setRe_passwoed(String re_passwoed) {
+        this.re_passwoed = re_passwoed;
+    }
+
+    
+    public void addNewPatient(int id, String FName, String LName, String gender, String birthDate, int age, String acceptedDate, String disease, String phone,String address) {
+
+        Patient patie = new Patient(id, FName, LName, gender, birthDate, age, acceptedDate, disease, phone,address);
+
+        if (patie.addPatient()) {
+
+            System.out.println(patie.toString() + "Patient Added Successfully ... !");
+
+        } else {
+
+            System.out.println("Patient Failed to insert ... !");
+
+        
+        }
+    
+    
+}
+    
+    
+    public void displayPatients() {
+
+        Patient patie = new Patient();
+
+        System.out.println(patie.displayAllPatients());
+
+    }
+
+    @Override
+    public String toString() {
+        return "Admins{" + "passwoed=" + passwoed + ", re_passwoed=" + re_passwoed + ", admins_FileName=" + admins_FileName + '}';
+    }
+
+   
+    
+    
+}
