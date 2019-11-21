dd636d59bc74b92a6cf41efe519adcb98f2c8a4e
ELFAROUQ <ELFAROUQ@DESKTOP-C92IOSP>
2019-11-21 13:42:25 +0200

    gui + 21 \11

--- Security_Project/src/security_project/Security_Project.java
+++ Security_Project/src/security_project/Security_Project.java
@@ -1,25 +1,29 @@
 
 package security_project;
 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 
 public class Security_Project {
 
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner input = new Scanner(System.in);
         
         Admins admins1 = new Admins("sandra", "george", "female", "13/7/1999", 20, "01024196555", "helwan", "0123456789", "0123456789");
         Admins admins2 = new Admins("mariam", "mostafa", "female", "13/7/1999", 20, "01024196555", "helwan", "0123456789", "0123456789");
         Admins admins3 = new Admins("rawan", "ashraf", "female", "13/7/1999", 20, "01024196555", "helwan", "0123456789", "0123456789");
-        
+        Patient patient =new Patient();
+        Admins admin=new Admins();
+          
         
         int ch=1;
         
         String FName,LName,gender,birthDate,phone,address,pass,re_pass,acc_date,disease;
         int id,age;
-        
+         System.out.print("Enter 1:Add "+"\n"+"2: display "+"\n"+"3: Search"+"\n"+"4: delete"+"\n");
+
         while(ch!=0){
             ch= input.nextInt();
             switch(ch){
@@ -51,7 +55,16 @@ public class Security_Project {
                 case 2:
                     admins1.displayPatients();
                     break;
-                    
+                case 3:
+                      System.out.print("\nSearch for Patient ...!\nEnter Patient ID : ");
+                        id = input.nextInt();
+                        admin.searchForPatient(id);
+                    break;
+                case 4:
+                        System.out.print("\nDelete Patient info ...!\nEnter Patient ID : ");
+                        id = input.nextInt();
+                        admin.deletePatient(id);
+                        break;
                 default:
                     System.out.println("Enter vaild Option :(");
             }
